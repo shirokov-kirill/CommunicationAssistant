a28e4460270faf73dea28ceb9f97a473ad17f4f7
Kirill Shirokov <ksshirokov01@gmail.com>
2024-01-15 22:37:30 +0100

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+import View.app
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        app()
+    }
+}
---
+++ src/main/kotlin/View/HelloWorld.kt
@@ -0,0 +1,22 @@
+package View
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+
+@Composable
+@Preview
+fun app() {
+    var text by remember { mutableStateOf("Hello, World!") }
+
+    MaterialTheme {
+        Button(onClick = {
+            text = "Hello, Desktop!"
+        }) {
+
+            Text(text)
+        }
+    }
+}
